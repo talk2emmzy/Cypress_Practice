0d242433345da99dab4118679f5e34ac219fbc2e
Emmanuel Okoye <talk2emmzy@gmail.com>
2023-05-28 21:13:10 +0100

    Added Assertions

--- cypress/e2e/1-getting-started/login.cy.js
+++ cypress/e2e/1-getting-started/login.cy.js
@@ -1,17 +1,57 @@
 // const cypress = require("cypress");
 // const { describe } = require("mocha");
 
+// const { assert } = require("chai")
+// const { expect } = require("chai")
+
 
 describe('Login Test Scenario', () =>{
 
-    it('verify that the user can login with a valid userid and password', () =>{
+    it('verify that the user can login with a valid userid and password - Implicit Assertion', () =>{
 
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') //visit url
+        cy.visit('https://opensource-demo.orangehrmlive.com') //visit url
+        
+        cy.url().should('include', 'orangehrmlive.com')
+        .and('contain', 'opensource')
+        .and('contain', 'https')
         
         cy.title().should('eq','OrangeHRM') //verify the Page Title
 
         .and('contain','HRM')//verify the page title contain
 
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+
+        cy.get('.oxd-button').click()
+
 
         })
+
+        it('verify that the user can login with a valid userid and password - Explicit Assertion', () =>{
+
+            cy.visit('https://opensource-demo.orangehrmlive.com') //visit url
+            
+            cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+
+            cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+
+            cy.get('.oxd-button').click()
+
+
+            let expName = "Paul Collings";
+
+            cy.get('.oxd-userdropdown-name').then( (E) =>{
+
+                let actName=E.text()
+
+                expect(actName).to.equal(expName) //BDD ASSERTION
+
+                assert.equal(actName,expName) //TDD ASSERTION
+
+            })
+    
+    
+        })
 })
+
