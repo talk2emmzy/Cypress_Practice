5a7b5af149a509b0fc06ecebb68e27522a85f46b
Emmanuel Okoye <talk2emmzy@gmail.com>
2023-05-28 14:38:18 +0100

    First Commit

---
+++ cypress/e2e/1-getting-started/login.cy.js
@@ -0,0 +1,17 @@
+const cypress = require("cypress");
+const { describe } = require("mocha");
+
+
+describe('Login Test Scenario', () =>{
+
+    it('verify that the user can login with a valid userid and password', () =>{
+
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') //visit url
+        
+        cy.title().should('eq','OrangeHRM') //verify the Page Title
+
+        .and('contain','HRM')//verify the page title contain
+
+        
+        })
+})
